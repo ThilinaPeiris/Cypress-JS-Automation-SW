c5b7f876c1221b966bdfb8be88475662b762a9fd
Bhagya Peiris <bhaggy91@gmail.com>
2023-12-06 17:30:47 +0530

    Added test "Verify that Cart Details are accurate"

--- cypress/e2e/shoppingCart.spec.js
+++ cypress/e2e/shoppingCart.spec.js
@@ -31,8 +31,33 @@ describe('Amazon Shopping Cart Test', () => {
     cy.selectProductRating(0);
     cy.selectProductLanguage('English');
     cy.navigateToAProductDetailsPage(2);
-    cy.getProductTitleinProductDetailsPage()
+
+    let productTitleInDetailsPage, ProductPriceInDetailsPage, subTotalInDetailsPage;
+
+    cy.get("#productTitle")
       .invoke('text')
+      .then((productTitle)=>{
+        productTitleInDetailsPage = productTitle; 
+      });
+
+      cy.get('li span a.a-button-text > span ').contains('Paperback').click();
+      
+      cy.get("li span a.a-button-text > span span.a-color-price")
+      .invoke('text')
+      .then((productPrice)=>{
+        ProductPriceInDetailsPage = productPrice; 
+      });
+
+      cy.get('select[name="quantity"] + span span.a-dropdown-prompt').click();
+      cy.get('li[aria-labelledby="quantity_2"]').click();
+      cy.get('#add-to-cart-button').click();
+
+      //goToCartBtn
+      cy.get('form + span  a.a-button-text').click();
+      
+      //deleteItemFromCart
+      cy.get("input[value='Delete']").click();
+
 
   })
 
