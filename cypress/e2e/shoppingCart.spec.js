1b941e7c304f52ba7745b1f46d330dc06738d5af
Bhagya Peiris <bhaggy91@gmail.com>
2023-12-07 12:02:27 +0530

    changes to complete the testcase.

--- cypress/e2e/shoppingCart.spec.js
+++ cypress/e2e/shoppingCart.spec.js
@@ -32,32 +32,56 @@ describe('Amazon Shopping Cart Test', () => {
     cy.selectProductLanguage('English');
     cy.navigateToAProductDetailsPage(2);
 
-    let productTitleInDetailsPage, ProductPriceInDetailsPage, subTotalInDetailsPage;
-
     cy.get("#productTitle")
       .invoke('text')
-      .then((productTitle)=>{
-        productTitleInDetailsPage = productTitle; 
+      .then((productTitleInDetailsPage) => {
+
+        cy.get('li span a.a-button-text > span ').contains('Paperback').click();
+
+        cy.get("li span a.a-button-text > span span.a-color-price")
+          .invoke('text')
+          .then((ProductPriceInDetailsPage) => {
+
+            let subTotalInDetailsPage = (Number(ProductPriceInDetailsPage.trim().substring(1))) * 2
+
+            cy.get('select[name="quantity"] + span span.a-dropdown-prompt').click();
+            cy.get('li[aria-labelledby="quantity_1"]').click();
+            cy.get('#add-to-cart-button').click();
+
+            //goToCartBtn
+            cy.get('form + span  a.a-button-text').click();
+
+            cy.get('span.a-list-item span.a-truncate-cut')
+              .invoke('text')
+              .then((productName) => {
+                productName = productName.trim().toLowerCase();
+                productTitleInDetailsPage = productTitleInDetailsPage.trim().toLocaleLowerCase()
+                expect(productName).equal(productTitleInDetailsPage);
+              });
+            cy.get('div.sc-badge-price-to-pay span')
+              .invoke('text')
+              .then((productPrice) => {
+                productPrice = productPrice.trim().toLowerCase();
+                ProductPriceInDetailsPage = ProductPriceInDetailsPage.trim().toLocaleLowerCase()
+                expect(productPrice).equal(ProductPriceInDetailsPage);
+              });
+            cy.get("span[id='sc-subtotal-amount-buybox'] span").invoke('text').should('contain', subTotalInDetailsPage);
+            cy.get("#sc-subtotal-label-buybox").invoke('text').should('contain', "2 item");
+
+            //deleteItemFromCart
+            cy.get("input[value='Delete']").click();
+
+            cy.get("#sc-subtotal-label-activecart")
+              .invoke('text')
+              .should('contain', "0 items");
+
+            cy.get("span[id='sc-subtotal-amount-activecart'] span")
+              .invoke('text')
+              .should('eq', "$0.00");
+
+          });
+
       });
-
-      cy.get('li span a.a-button-text > span ').contains('Paperback').click();
-      
-      cy.get("li span a.a-button-text > span span.a-color-price")
-      .invoke('text')
-      .then((productPrice)=>{
-        ProductPriceInDetailsPage = productPrice; 
-      });
-
-      cy.get('select[name="quantity"] + span span.a-dropdown-prompt').click();
-      cy.get('li[aria-labelledby="quantity_2"]').click();
-      cy.get('#add-to-cart-button').click();
-
-      //goToCartBtn
-      cy.get('form + span  a.a-button-text').click();
-      
-      //deleteItemFromCart
-      cy.get("input[value='Delete']").click();
-
 
   })
 
